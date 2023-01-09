757168aa8a7420c203d94dbccbec0e7688a34743
Cristóbal Armaza <ca455@cornell.edu>
2023-01-09 11:18:34 -0500

    Add more dox to RandomDistribution

--- nbpy/random_distribution.py
+++ nbpy/random_distribution.py
@@ -1,14 +1,25 @@
 # Distributed under the MIT License.
 # See LICENSE for details.
+"""
+Defines the `RandomDistribution` class.
+"""
 
 import numpy as np
 
 
 class RandomDistribution:
     """
-    Set positions and velocities to random values normally distributed.
-    The normal distribution is fed the given `seed`.
+    Sets positions and velocities to random values normally distributed.
 
+    Attributes
+    ----------
+    seed : int (default: 25092020)
+    The RNG seed.
+
+    Functions
+    ---------
+    set_variables(positions, velocities)
+    Assigns positions and velocities to random numbers.
     """
 
     def __init__(self, seed=25092020):
@@ -16,9 +27,21 @@ class RandomDistribution:
 
     @property
     def seed(self):
+        """
+        The RNG seed used to generate the distribution.
+        """
         return self._seed
 
     def set_variables(self, positions, velocities):
+        """
+        Assign `positions` and `velocities` to random numbers.
+
+        Parameters
+        ----------
+        `positions, velocities` : ndarray, ndarray
+        The N positions and velocities, represented as N-by-3 numpy arrays.
+
+        """
         N = positions.shape[0]
         rng = np.random.default_rng(self._seed)
         positions[:] = rng.standard_normal((N, 3))
