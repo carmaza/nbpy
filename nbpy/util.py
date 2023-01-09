cf59a98459a4307eba3968f17c1f90f53c877353
Cristóbal Armaza <ca455@cornell.edu>
2023-01-09 11:19:21 -0500

    Document util and use f-strings

--- nbpy/util.py
+++ nbpy/util.py
@@ -1,16 +1,31 @@
 # Distributed under the MIT License.
 # See LICENSE for details.
+"""
+Includes the following utility functions:
+
+create_folder(folder)
+Creates a folder and throws exception if it can't.
+"""
 
 import errno
 import os
 
 
-def create_folder(folder):
+def create_folder(path: str):
+    """
+    Create folder given the path.
+
+    Parameters
+    ----------
+
+    path : str
+    The path to the new folder, including its name.
+    """
     try:
-        os.makedirs("./{}".format(folder))
-        print("Local folder /{} created.".format(folder))
-    except OSError as e:
-        if (e.errno == errno.EEXIST):
-            print("Local folder /{} already exists.".format(folder))
+        os.makedirs(f"./{path}")
+        print(f"Local folder /{path} created.")
+    except OSError as err:
+        if err.errno == errno.EEXIST:
+            print(f"Local folder /{path} already exists.")
         else:
             raise
