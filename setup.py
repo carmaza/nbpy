65f5e1d241fe1f20d399fcbda02bb7488b040d17
Cristóbal Armaza <ca455@cornell.edu>
2023-01-09 12:18:22 -0500

    Run yapf on root

--- setup.py
+++ setup.py
@@ -6,19 +6,18 @@ from setuptools import setup, find_packages
 with open('README.md', 'r') as readme_file:
     long_description = readme_file.read()
 
-setup(
-    name='nbpy',
-    version='0.1.0',
-    description="Recreational N-body problem solver.",
-    long_description=long_description,
-    url='https://github.com/carmaza/nbpy',
-    author='Cristóbal Armaza',
-    author_email='ca455@cornell.edu',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ],
-    keywords='astrophysics galactic n-body newton gravity',
-    packages=find_packages(),
-    install_requires=['matplotlib>=3.6.0', 'numpy>=1.23.0'],
-    python_requires='>=3.10')
+setup(name='nbpy',
+      version='0.1.0',
+      description="Recreational N-body problem solver.",
+      long_description=long_description,
+      url='https://github.com/carmaza/nbpy',
+      author='Cristóbal Armaza',
+      author_email='ca455@cornell.edu',
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent',
+      ],
+      keywords='astrophysics galactic n-body newton gravity',
+      packages=find_packages(),
+      install_requires=['matplotlib>=3.6.0', 'numpy>=1.23.0'],
+      python_requires='>=3.10')
