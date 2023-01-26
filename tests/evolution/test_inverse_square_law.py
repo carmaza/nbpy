8a2c30e716e8f75af6a1fd26fa0303bfac64a899
Cristóbal Armaza <ca455@cornell.edu>
2023-01-26 00:59:44 -0500

    Split tests

--- tests/evolution/test_inverse_square_law.py
+++ tests/evolution/test_inverse_square_law.py
@@ -18,25 +18,44 @@ class TestInverseSquareLaw(unittest.TestCase):
 
     """
 
-    def test(self):
+    @classmethod
+    def setUpClass(cls):
+        cls._seed = np.random.randint(0, 1e6)
+        np.random.seed(cls._seed)
+
+        cls._constant = np.random.rand()
+        cls._softening = np.random.rand()
+        cls._law = InverseSquareLaw(cls._constant, cls._softening)
+
+    def test_attribute_interface(self):
         """
-        Test class and member functions.
+        Test interface for class attributes.
 
         """
-        seed = np.random.randint(0, 1e6)
-        np.random.seed(seed)
+        self.assertAlmostEqual(
+            self._constant,
+            self._law.constant,
+            msg="value of constant differs from expected value. "
+            f"RNG seed: {self._seed}.")
 
+        self.assertAlmostEqual(
+            self._softening,
+            self._law.softening,
+            msg="value of softening differs from expected value. "
+            f"RNG seed: {self._seed}.")
+
+    def test_exert(self):
+        """
+        Test implementation of member function `exert`.
+
+        """
         dim = 3
         n_body = np.random.randint(2, 10)
         positions = np.random.randn(n_body, dim)
         masses = np.random.rand(n_body)
 
-        constant = np.random.rand()
-        softening = np.random.rand()
-        law = InverseSquareLaw(constant, softening)
-
         accelerations = np.empty_like(positions)
-        law.exert(accelerations, masses, positions)
+        self._law.exert(accelerations, masses, positions)
 
         accelerations_expected = np.zeros_like(positions)
 
@@ -45,11 +64,12 @@ class TestInverseSquareLaw(unittest.TestCase):
                 d_x = positions[k, 0] - positions[j, 0]
                 d_y = positions[k, 1] - positions[j, 1]
                 d_z = positions[k, 2] - positions[j, 2]
-                d_cube = (d_x**2. + d_y**2. + d_z**2. + softening**2.)**1.5
+                d_cube = (d_x**2. + d_y**2. + d_z**2. +
+                          self._softening**2.)**1.5
                 accelerations_expected[j, 0] += mass * d_x / d_cube
                 accelerations_expected[j, 1] += mass * d_y / d_cube
                 accelerations_expected[j, 2] += mass * d_z / d_cube
 
         self.assertTrue(np.allclose(accelerations, accelerations_expected),
                         msg="acceleration differs from expected value. "
-                        f"RNG seed: {seed}.")
+                        f"RNG seed: {self._seed}.")
