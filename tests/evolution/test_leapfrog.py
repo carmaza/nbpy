86dc53198a9d5017edf5c38cf4c1b61d73f9aa89
Cristóbal Armaza <ca455@cornell.edu>
2023-01-26 01:00:16 -0500

    Add test setup

--- tests/evolution/test_leapfrog.py
+++ tests/evolution/test_leapfrog.py
@@ -18,26 +18,29 @@ class TestLeapfrog(unittest.TestCase):
 
     """
 
-    def test(self):
+    @classmethod
+    def setUpClass(cls):
+        cls._seed = np.random.randint(0, 1e6)
+        np.random.seed(cls._seed)
+
+        cls._stepper = Leapfrog()
+
+        # The specific law we use is unimportant.
+        cls._law = InverseSquareLaw(np.random.rand(), np.random.rand())
+
+    def test_evolve(self):
         """
-        Test class and member functions.
+        Test implementation of member function `evolve`.
 
         """
-
-        seed = np.random.randint(0, 1e6)
-        np.random.seed(seed)
-
         dim = 3
         N = np.random.randint(2, 10)
         masses = np.random.rand(N)
 
-        # The specific law we use is unimportant.
-        law = InverseSquareLaw(np.random.rand(), np.random.rand())
-
         pos = np.random.randn(N, dim)
         vel = np.random.randn(N, dim)
         acc = np.empty_like(pos)
-        law.exert(acc, masses, pos)
+        self._law.exert(acc, masses, pos)
 
         # Copies to compare with later.
         pos_ini = pos.copy()
@@ -46,25 +49,24 @@ class TestLeapfrog(unittest.TestCase):
 
         # Update evolved variables.
         dt = np.random.randn()
-        stepper = Leapfrog()
-        stepper.evolve(pos, vel, acc, dt, masses, law)
+        self._stepper.evolve(pos, vel, acc, dt, masses, self._law)
 
         # Construct expected evolved variables.
         v_halfstep = vel_ini + 0.5 * dt * acc_ini
         pos_expected = pos_ini + dt * v_halfstep
 
         acc_fin = np.empty_like(pos_expected)
-        law.exert(acc_fin, masses, pos_expected)
+        self._law.exert(acc_fin, masses, pos_expected)
         vel_expected = v_halfstep + 0.5 * dt * acc_fin
 
         self.assertTrue(np.allclose(pos, pos_expected),
                         msg="new position differs from expected value. "
-                        f"RNG seed: {seed}.")
+                        f"RNG seed: {self._seed}.")
 
         self.assertTrue(np.allclose(vel, vel_expected),
                         msg="new velocity differs from expected value. "
-                        f"RNG seed: {seed}.")
+                        f"RNG seed: {self._seed}.")
 
         self.assertTrue(np.allclose(acc, acc_fin),
                         msg="new acceleration differs from expected value. "
-                        f"RNG seed: {seed}.")
+                        f"RNG seed: {self._seed}.")
